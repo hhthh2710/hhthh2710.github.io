6de70f30cc654a1b15449f8af6af86f7ab6c6f64
hhthh2710 <huyhoang9461@gmail.com>
2024-10-10 10:24:13 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function show(){
+    x = document.getElementById('pig');
+    x.style.display = "block";
+}
+
+function hide(){
+    x = document.getElementById("pig");
+    x.style.display = 'none';
+}
